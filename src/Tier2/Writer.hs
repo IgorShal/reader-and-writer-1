88edb70677c0a6dbd9a5c4b8374a1d23a9158079
IgorShal <125886998+IgorShal@users.noreply.github.com>
2023-03-06 23:37:33 +0700

    commit

--- src/Tier2/Writer.hs
+++ src/Tier2/Writer.hs
@@ -6,9 +6,14 @@ import Tier0.Writer (Tree (..))
 first (a,b) = a
 second (a,b) = b
 collectAndSumInOrder :: Num a => Tree a -> Writer (Sum a) [a]
-collectAndSumInOrder (Leaf x) = writer ([x],x)
-collectAndSumInOrder (Branch l v r) = 
-  writer $ (first (runWriter (collectAndSumInOrder l)),second (runWriter (collectAndSumInOrder l)))
+collectAndSumInOrder (Leaf x) = do
+    tell (Sum x)
+    return [x]
+collectAndSumInOrder (Branch l v r) = do
+    x <- collectAndSumInOrder l
+    tell (Sum v)
+    y <- collectAndSumInOrder r
+    return (x++[v]++y)
 
  
   
