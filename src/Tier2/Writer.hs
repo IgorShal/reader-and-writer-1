c906f20b11fa0b4e8fe638416811808dcee83956
IgorShal <125886998+IgorShal@users.noreply.github.com>
2023-03-06 23:00:53 +0700

    commit

--- src/Tier2/Writer.hs
+++ src/Tier2/Writer.hs
@@ -3,5 +3,12 @@ module Tier2.Writer (collectAndSumInOrder) where
 import Control.Monad.Writer
 import Tier0.Writer (Tree (..))
 
+first (a,b) = a
+second (a,b) = b
 collectAndSumInOrder :: Num a => Tree a -> Writer (Sum a) [a]
-collectAndSumInOrder = undefined
+collectAndSumInOrder (Leaf x) = writer ([x],x)
+collectAndSumInOrder (Branch l v r) = 
+  writer $ (first (runWriter (collectAndSumInOrder l)),second (runWriter (collectAndSumInOrder l)))
+
+ 
+  
